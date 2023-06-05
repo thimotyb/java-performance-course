a6b927c2b07600bd2e9e26657067a39e9c2c43ce
thimoty barbieri <thimoty@thimoty.it>
2023-06-05 20:34:37 +0000

    Added Scratch

---
+++ Scratch.java
@@ -0,0 +1,15 @@
+public class Scratch {
+    private int i;
+ 
+    public Scratch() {
+        i = 0;
+    }
+ 
+    public int getI() {
+        return i;
+    }
+ 
+    public void setI(int i) {
+        this.i = i;
+    }
+}
